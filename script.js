2f7acfe6623c9d06748d7d507c9ebc33c09bd899
George Nicholson <ga.nicholson@outlook.com>
2023-03-26 19:06:01 +0100

    added styling template and updated the template so js code works as should

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-    const database =[
-        {
-            username : "George",
-            password : "pass1"
-        },
-        {
-            username : "Amy",
-            password : "pass2"
-        },
-        {
-            username : "Luka",
-            password : "pass3"
-        }
-    ];
+const database =[
+    {
+        username : "George",
+        password : "pass1"
+    },
+    {
+        username : "Amy",
+        password : "pass2"
+    },
+    {
+        username : "Luka",
+        password : "pass3"
+    }
+];
 
-const userName = document.getElementById('userName').value
-const userPassword = document.getElementById('userPassword').value
+const userName = document.getElementById('userName').value;
+const userPassword = document.getElementById('userPassword').value;
 const form = document.getElementById('form')
 
-   
+
 
 
 function isUserValid() {
-    const userName = document.getElementById('userName').value;
-    const userPassword = document.getElementById('userPassword').value;
-  
-    for (var i = 0; i < database.length; i++) {
-      if (database[i].username === userName && database[i].password === userPassword) {
-        return true;
-      }
-    }
-    return false;
+const userName = document.getElementById('userName').value;
+const userPassword = document.getElementById('userPassword').value;
+
+for (var i = 0; i < database.length; i++) {
+  if (database[i].username === userName && database[i].password === userPassword) {
+    return true;
   }
-  
+}
+return false;
+}
+
 isUserValid(userName,userPassword);
 
 form.addEventListener('submit', (e) => {
-    if(isUserValid(userName,userPassword)){
-        alert("welcome")
-    }else{alert("wrong username or password")}
-    e.preventDefault()
+if(isUserValid(userName,userPassword)){
+    alert("welcome")
+}else{alert("wrong username or password")}
+e.preventDefault()
 })
